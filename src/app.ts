1c0c4a290147d61200ac8b1f4db28e1ba9cdb9ce
Yash singhal <109813515+yashsinghal28@users.noreply.github.com>
2024-08-21 22:52:21 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: ["https://www.ecommercesite.me"],
+    origin: ["https://ecommercefrontend-peach.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
